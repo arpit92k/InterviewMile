336b574569bfe219ce35ce4b1f05e52c2b81be9f
arpit92k <arpit92k@gmail.com>
2015-05-31 18:14:33 +0530

    updated api classes

--- api/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'classes/class.database.php';
-include_once 'classes/class.databaseConnection.php';
-$database=new Database();
-$database->setQuery("INSERT INTO categories(`category`) VALUES (?)");
-$database->bindParms(array("base category"));
-print_r($database->executeInsertQuery());
-echo "aks";
-?>
